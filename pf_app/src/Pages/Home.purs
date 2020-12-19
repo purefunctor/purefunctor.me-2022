8ca33adff435891922a0ee3e5f213361cd5a29c6
PureFunctor <purefunctor@gmail.com>
2020-12-19 18:41:32 +0800

    [PURS] Add rudimentary styling for the Home page

--- pf_app/src/Pages/Home.purs
+++ pf_app/src/Pages/Home.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Halogen as H
 import Halogen.HTML as HH
+import PF.Component.Utils (css)
 
 
 type State = Unit
@@ -24,8 +25,16 @@ initialState _ = unit
 
 render :: forall action m. State -> H.ComponentHTML action () m
 render _ =
-  HH.div_
-  [ HH.h1_
-    [ HH.text "Home Page"
+  HH.div [ css "flex flex-col h-screen p-5 bg-gradient-to-br from-green-500 to-blue-900" ]
+  [ HH.div [ css "grid grid-cols-2 grid-rows-2 flex-1 gap-5" ]
+    [ HH.div [ css "box-content row-span-2 p-5 text-left text-6xl bg-opacity-20 bg-gray-900 border-4 border-gray-900" ]
+      [ HH.text "PureFunctor"
+      ]
+    , HH.div [ css "box-content text-left p-5 text-6xl bg-opacity-20 bg-gray-900 border-4 border-gray-900" ]
+      [ HH.text "Projects"
+      ]
+    , HH.div [ css "box-content text-left p-5 text-6xl bg-opacity-20 bg-gray-900 border-4 border-gray-900" ]
+      [ HH.text "Socials"
+      ]
     ]
   ]
