9d86e1a8738d2ed974f676a9f6cf84df90b5faeb
PureFunctor <purefunctor@gmail.com>
2020-12-19 15:50:35 +0800

    [PURS] Use the Router component as the entry point

--- pf_app/src/Main.purs
+++ pf_app/src/Main.purs
@@ -2,9 +2,22 @@ module Main where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+import PF.Component.Router as Router
+import PF.Data.Routes as Routes
+import Routing.Duplex (parse)
+import Routing.Hash (matchesWith)
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  halogenIO <- runUI Router.component unit body
+  void $ liftEffect $ matchesWith (parse Routes.routeCodec) \old new ->
+    when (old /= Just new) do
+      launchAff_ $ halogenIO.query $ H.tell $ Router.Navigate new
