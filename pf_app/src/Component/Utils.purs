6c955241de2079c14bf3a8ec27a3c7ff158f26df
PureFunctor <purefunctor@gmail.com>
2020-12-19 14:34:46 +0800

    [PURS] Add function for applying CSS classes

--- pf_app/src/Component/Utils.purs
+++ pf_app/src/Component/Utils.purs
@@ -1,1 +1,11 @@
 module PF.Component.Utils where
+
+import Prelude
+
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+
+
+-- | Utility function for applying CSS classes
+css :: forall r i. String -> HP.IProp (class :: String | r) i
+css = HP.class_ <<< HH.ClassName
