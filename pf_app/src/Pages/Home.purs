40800f70f6ac5f147fe38bb928a41637990d9a3c
PureFunctor <purefunctor@gmail.com>
2020-12-19 19:21:04 +0800

    [PURS] Add generalized HTML functions for Home

--- pf_app/src/Pages/Home.purs
+++ pf_app/src/Pages/Home.purs
@@ -2,6 +2,7 @@ module PF.Pages.Home where
 
 import Prelude
 
+import Data.List (intercalate)
 import Halogen as H
 import Halogen.HTML as HH
 import PF.Component.Utils (css)
@@ -25,16 +26,44 @@ initialState _ = unit
 
 render :: forall action m. State -> H.ComponentHTML action () m
 render _ =
-  HH.div [ css "flex flex-col h-screen p-5 bg-gradient-to-br from-green-500 to-blue-900" ]
-  [ HH.div [ css "grid grid-cols-2 grid-rows-2 flex-1 gap-5" ]
-    [ HH.div [ css "box-content row-span-2 p-5 text-left text-6xl bg-gray-200 border-4 border-gray-900" ]
+  pageRoot
+  [ tileGrid
+    [ sTile [ "row-span-2" ]
       [ HH.text "PureFunctor"
       ]
-    , HH.div [ css "box-content text-left p-5 text-6xl bg-gray-200 border-4 border-gray-900" ]
+    , dTile
       [ HH.text "Projects"
       ]
-    , HH.div [ css "box-content text-left p-5 text-6xl bg-gray-200 border-4 border-gray-900" ]
+    , dTile
       [ HH.text "Socials"
       ]
     ]
   ]
+  where
+  -- | Root container for the page
+  pageRoot = HH.div [ css "flex flex-col h-screen p-5 bg-gradient-to-br from-green-500 to-blue-900" ]
+
+  -- | Parent container for the tiles
+  tileGrid = HH.div [ css "grid grid-cols-2 grid-rows-2 flex-1 gap-5" ]
+
+  -- | Tile factory function
+  tile_ styles properties =
+    HH.div $ [ css $ intercalate " " $
+               [ "box-content"
+               , "text-left"
+               , "p-5"
+               , "text-6xl"
+               , "bg-gray-200"
+               , "border-4"
+               , "border-gray-900"
+               ] <> styles
+             ]
+
+  -- | Tile with extra styles
+  sTile = flip tile_ []
+
+  -- | Tile with extra properties
+  pTile = tile_ []
+
+  -- | Tile with no extras
+  dTile = tile_ [] []
