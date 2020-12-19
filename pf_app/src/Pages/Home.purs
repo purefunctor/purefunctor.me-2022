7734c62d7e8e5db59bcfe2ae7c915842ffb85265
PureFunctor <purefunctor@gmail.com>
2020-12-19 18:59:04 +0800

    [PURS] Make background color of tiles white

--- pf_app/src/Pages/Home.purs
+++ pf_app/src/Pages/Home.purs
@@ -27,13 +27,13 @@ render :: forall action m. State -> H.ComponentHTML action () m
 render _ =
   HH.div [ css "flex flex-col h-screen p-5 bg-gradient-to-br from-green-500 to-blue-900" ]
   [ HH.div [ css "grid grid-cols-2 grid-rows-2 flex-1 gap-5" ]
-    [ HH.div [ css "box-content row-span-2 p-5 text-left text-6xl bg-opacity-20 bg-gray-900 border-4 border-gray-900" ]
+    [ HH.div [ css "box-content row-span-2 p-5 text-left text-6xl bg-gray-200 border-4 border-gray-900" ]
       [ HH.text "PureFunctor"
       ]
-    , HH.div [ css "box-content text-left p-5 text-6xl bg-opacity-20 bg-gray-900 border-4 border-gray-900" ]
+    , HH.div [ css "box-content text-left p-5 text-6xl bg-gray-200 border-4 border-gray-900" ]
       [ HH.text "Projects"
       ]
-    , HH.div [ css "box-content text-left p-5 text-6xl bg-opacity-20 bg-gray-900 border-4 border-gray-900" ]
+    , HH.div [ css "box-content text-left p-5 text-6xl bg-gray-200 border-4 border-gray-900" ]
       [ HH.text "Socials"
       ]
     ]
