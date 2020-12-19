6a3f5253cb62db208ddf281877c20cca7e6a7f3a
PureFunctor <purefunctor@gmail.com>
2020-12-19 15:49:48 +0800

    [PURS] Define application routes and codec

--- pf_app/src/Data/Routes.purs
+++ pf_app/src/Data/Routes.purs
@@ -1,1 +1,22 @@
 module PF.Data.Routes where
+
+import Data.Eq (class Eq)
+import Data.Function (($))
+import Data.Generic.Rep (class Generic)
+import Routing.Duplex (RouteDuplex', root)
+import Routing.Duplex.Generic (noArgs, sum)
+import Routing.Duplex.Generic.Syntax ((/))
+
+
+data Routes = HomeR | AboutR
+
+
+derive instance genericRoutes :: Generic Routes _
+derive instance eqRoutes :: Eq Routes
+
+
+routeCodec :: RouteDuplex' Routes
+routeCodec = root $ sum
+  { "HomeR": noArgs
+  , "AboutR": "about" / noArgs
+  }
