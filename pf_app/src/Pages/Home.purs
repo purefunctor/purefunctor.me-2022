8b7744050bcf91ac9e4b06dac88d6839e4477bca
PureFunctor <purefunctor@gmail.com>
2020-12-19 14:35:09 +0800

    [PURS] Add templates for the Home and About pages

--- pf_app/src/Pages/Home.purs
+++ pf_app/src/Pages/Home.purs
@@ -1,1 +1,31 @@
 module PF.Pages.Home where
+
+import Prelude
+
+import Halogen as H
+import Halogen.HTML as HH
+
+
+type State = Unit
+
+
+component :: forall query input output m. H.Component HH.HTML query input output m
+component =
+  H.mkComponent
+  { initialState
+  , render
+  , eval: H.mkEval $ H.defaultEval
+  }
+
+
+initialState :: forall input. input -> Unit
+initialState _ = unit
+
+
+render :: forall action m. State -> H.ComponentHTML action () m
+render _ =
+  HH.div_
+  [ HH.h1_
+    [ HH.text "Home Page"
+    ]
+  ]
