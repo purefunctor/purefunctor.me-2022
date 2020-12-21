1ccda8c9b0f730bd4b3724a4cf63125967553438
PureFunctor <purefunctor@gmail.com>
2020-12-21 21:54:41 +0800

    [NODE] Configure Google Fonts for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Montserrat']
+    },
     extend: {},
   },
   variants: {
