3799109e6156ddc20b30fc76430366ccbc947956
PureFunctor <purefunctor@gmail.com>
2021-01-16 20:58:08 +0800

    Add initial layout for the home page

--- pf_app/src/Pages/Home.purs
+++ pf_app/src/Pages/Home.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Halogen as H
 import Halogen.HTML as HH
+import PF.Component.Utils (css)
 
 
 type State = Unit
@@ -24,8 +25,18 @@ initialState _ = unit
 
 render :: forall action m. State -> H.ComponentHTML action () m
 render _ =
-  HH.div_
-  [ HH.h1_
-    [ HH.text "Home Page"
+  HH.div [ css "flex h-screen w-screen bg-gray-100" ]
+  [ HH.div [ css "bg-gray-200 h-full w-full lg:w-11/12 mx-auto shadow-xl" ]
+    [ HH.div [ css "flex flex-col h-full justify-center items-center space-y-5" ]
+      [ HH.div [ css "h-56 w-56 bg-green-200 rounded-full shadow-xl" ]
+        [
+        ]
+      , HH.div [ css "text-4xl text-center" ]
+        [ HH.text "PureFunctor"
+        ]
+      , HH.div [ css "text-4xl font-thin text-center" ]
+        [ HH.text "Student, Python, FP"
+        ]
+      ]
     ]
   ]
