95e78ee0792083c600618b321132aeeca7962ed5
PureFunctor <purefunctor@gmail.com>
2021-01-18 11:25:05 +0800

    Generate shell.nix using cabal2nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,32 @@
-{ pkgs ? import <nixpkgs> {}
-, compiler ? "ghc884"
-}:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc884", doBenchmark ? false }:
+
 let
-  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (pkgs: with pkgs; [aeson base persistent yesod]);
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, persistent, stdenv, yesod }:
+      mkDerivation {
+        pname = "purefunctor-me";
+        version = "1.0.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [ aeson base persistent yesod ];
+        executableHaskellDepends = [ base ];
+        testHaskellDepends = [ base ];
+        homepage = "https://github.com/PureFunctor/purefunctor.me";
+        description = "My personal portfolio website written in PureScript and Haskell";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
 in
-  pkgs.mkShell {
-    buildInputs = [ ghc ];
-  }
+
+  if pkgs.lib.inNixShell then drv.env else drv
