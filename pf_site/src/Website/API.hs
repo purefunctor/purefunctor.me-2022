72df19c6832eb5fefcf84441fbbed3ea46729a95
PureFunctor <purefunctor@gmail.com>
2021-01-21 19:36:25 +0800

    Refactor the example app to use Sqlite

--- pf_site/src/Website/API.hs
+++ pf_site/src/Website/API.hs
@@ -5,10 +5,13 @@ module Website.API where
 
 
 import Control.Monad.IO.Class
+import Control.Monad.Logger
 import Data.Proxy
 import Data.Time
 import Data.Time.Calendar.Julian
+import Database.Persist.Sqlite
 import Network.Wai.Handler.Warp
+import Servant
 import Servant.API
 import Servant.Server
 import Website.Models
@@ -18,19 +21,48 @@ type BlogAPI =
   "blog" :> Capture "blogid" Int :> Get '[JSON] BlogPost
 
 
-handleBlogPost :: Handler BlogPost
-handleBlogPost = do
-  time <- liftIO getCurrentTime
-  return $ BlogPost "Haskell Is Simple" "haskell-is-simple" "SOON™" time time
+
+getBlogPost :: ConnectionPool -> IO (Maybe BlogPost)
+getBlogPost pool = flip runSqlPersistMPool pool $ do
+    post <- selectFirst [ BlogPostShortTitle ==. "haskell-is-simple" ] [ ]
+    return $ case post of
+      (Just post') -> Just . entityVal $ post'
+      Nothing      -> Nothing
+
+
+getBlogPostH :: ConnectionPool -> Handler BlogPost
+getBlogPostH pool = do
+    post <- liftIO $ getBlogPost pool
+    case post of
+       (Just post') -> return post'
+       Nothing      -> throwError err404
 
 
 blogAPI :: Proxy BlogAPI
 blogAPI = Proxy
 
 
-blogServer :: Server BlogAPI
-blogServer = return handleBlogPost
+blogServer :: ConnectionPool -> Server BlogAPI
+blogServer = return . getBlogPostH
 
 
-blogApp :: Application
-blogApp = serve blogAPI blogServer
+blogApp :: ConnectionPool -> Application
+blogApp = serve blogAPI . blogServer
+
+
+mkBlogApp :: IO Application
+mkBlogApp = do
+    pool <- runStderrLoggingT $ do
+        createSqlitePool "db.sqlite" 5
+
+    time <- getCurrentTime
+
+    flip runSqlPool pool $ do
+        runMigration migrateAll
+        insert $ BlogPost "Haskell Is Simple" "haskell-is-simple" "SOON™" time time
+
+    return $ blogApp pool
+
+
+debug :: IO ()
+debug = run 3000 =<< mkBlogApp
