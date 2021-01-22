4ed524f3909adf3e5023d5bfa5ddc720fdac07b2
PureFunctor <purefunctor@gmail.com>
2021-01-22 23:06:50 +0800

    Create example handlers for POST requests

--- pf_site/src/Website/API.hs
+++ pf_site/src/Website/API.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE TypeOperators     #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -6,22 +7,46 @@ module Website.API where
 
 import Control.Monad.IO.Class
 import Control.Monad.Logger
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Proxy
 import Data.Text
 import Data.Time
 import Database.Persist.Sqlite
+import GHC.Generics
 import Network.Wai.Handler.Warp
 import Servant
 import Website.Models
 
 
+data PostInfo = PostInfo
+  { title   :: Text
+  , content :: Text
+  } deriving Generic
+
+
+instance FromJSON PostInfo
+instance ToJSON PostInfo
+
+
+data RepoInfo = RepoInfo
+  { name  :: Text
+  , owner :: Text
+  } deriving Generic
+
+
+instance FromJSON RepoInfo
+instance ToJSON RepoInfo
+
+
 type BlogAPI =
   "blog" :> "all-posts" :> Get '[JSON] [BlogPost] :<|>
+  "blog" :> "create" :> ReqBody '[JSON] PostInfo :> Post '[JSON] BlogPost :<|>
   "blog" :> Capture "post-name" Text :> Get '[JSON] BlogPost
 
 
 type RepoAPI =
   "repo" :> "all-repos" :> Get '[JSON] [Repository] :<|>
+  "repo" :> "create" :> ReqBody '[JSON] RepoInfo :> Post '[JSON] Repository :<|>
   "repo" :> Capture "repo-name" Text :> Get '[JSON] Repository
 
 
@@ -45,6 +70,20 @@ getAllPostsH pool = do
   return $ entityVal <$> posts
 
 
+postBlogPostH :: ConnectionPool -> PostInfo -> Handler BlogPost
+postBlogPostH pool info = do
+  now <- liftIO getCurrentTime
+
+  let title'   = title info
+  let short    = Data.Text.replace " " "_" title'
+  let content' = content info
+
+  let post     = BlogPost title' short content' now now
+
+  _ <- liftIO $ flip runSqlPersistMPool pool $ insert post
+
+  return post
+
 
 getRepoInfoH :: ConnectionPool -> Text -> Handler Repository
 getRepoInfoH pool repoName = do
@@ -63,6 +102,18 @@ getAllReposH pool = do
   return $ entityVal <$> repos
 
 
+postRepoInfoH :: ConnectionPool -> RepoInfo -> Handler Repository
+postRepoInfoH pool info = do
+  let name'  = name info
+  let owner' = owner info
+  let url    = Data.Text.concat [ "https://github.com" , "/" , name' , "/" , owner' ]
+  let repo   = Repository name' owner' url 0 0
+
+  _ <- liftIO $ flip runSqlPersistMPool pool $ insert repo
+
+  return repo
+
+
 siteAPI :: Proxy SiteAPI
 siteAPI = Proxy
 
@@ -70,8 +121,8 @@ siteAPI = Proxy
 siteServer :: ConnectionPool -> Server SiteAPI
 siteServer pool = blogServer :<|> repoServer
   where
-    blogServer = getAllPostsH pool :<|> getBlogPostH pool 
-    repoServer = getAllReposH pool :<|> getRepoInfoH pool 
+    blogServer = getAllPostsH pool :<|> postBlogPostH pool :<|> getBlogPostH pool
+    repoServer = getAllReposH pool :<|> postRepoInfoH pool :<|> getRepoInfoH pool
 
 
 siteApp :: ConnectionPool -> Application
@@ -80,7 +131,7 @@ siteApp = serve siteAPI . siteServer
 
 mkSiteApp :: IO Application
 mkSiteApp = do
-  pool <- runStderrLoggingT $ do
+  pool <- runStderrLoggingT $
     createSqlitePool "db.sqlite" 5
 
   time <- getCurrentTime
