57f7874bcfc151da503905101f33b19bbf851601
PureFunctor <purefunctor@gmail.com>
2021-01-24 17:09:27 +0800

    Implement the mkWebsiteApp_ and debug functions

--- pf_site/src/Website/API.hs
+++ pf_site/src/Website/API.hs
@@ -117,7 +117,9 @@ websiteApp = serve (Proxy :: Proxy WebsiteAPI) . websiteServer
 
 
 mkWebsiteApp_ :: IO (ConnectionPool, Application)
-mkWebsiteApp_ = undefined
+mkWebsiteApp_ = do
+  pool <- runStderrLoggingT $ createSqlitePool "database.sqlite" 2
+  return (pool, websiteApp pool)
 
 
 mkWebsiteApp :: IO Application
@@ -127,4 +129,14 @@ mkWebsiteApp = snd <$> mkWebsiteApp_
 debug :: IO ()
 debug = do
   (pool, app) <- mkWebsiteApp_
-  return ()
+
+  now <- getCurrentTime
+
+  flip runSqlPool pool $ do
+    runMigration migrateAll
+    insert $ BlogPost "Haskell Is Simple" "haskell-is-simple" "SOON™" now now
+    insert $ BlogPost "Python Is Awesome" "python-is-awesome" "SOON™" now now
+    insert $ Repository "amalgam-lisp" "PureFunctor" "https://github.com/PureFunctor/amalgam-lisp" 0 0
+    insert $ Repository "purefunctor.me" "PureFunctor" "https://github.com/PureFunctor/purefunctor.me" 0 0
+
+  void $ run 3000 app
