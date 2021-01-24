772309b76744db31cec5920f8cd61a4b7ac6834b
PureFunctor <purefunctor@gmail.com>
2021-01-24 16:31:11 +0800

    Refactor API structure and add function skeletons

--- pf_site/src/Website/API.hs
+++ pf_site/src/Website/API.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE TypeOperators     #-}
-{-# LANGUAGE OverloadedStrings #-}
 module Website.API where
 
 
+import Control.Monad (void)
 import Control.Monad.IO.Class
 import Control.Monad.Logger
 import Data.Aeson (FromJSON, ToJSON)
@@ -18,133 +19,60 @@ import Servant
 import Website.Models
 
 
-data PostInfo = PostInfo
+type BlogPostAPI =
+  "post" :> Get '[JSON] [BlogPost] :<|>
+  "post" :> Capture "short-title" Text :> Get '[JSON] BlogPost :<|>
+  "post" :> ReqBody '[JSON] CreateBlogPostData :> Post '[JSON] BlogPost
+
+
+data CreateBlogPostData = CreateBlogPostData
   { title   :: Text
   , content :: Text
-  } deriving Generic
+  } deriving (Generic, FromJSON, ToJSON)
 
 
-instance FromJSON PostInfo
-instance ToJSON PostInfo
+blogPostServer :: ConnectionPool -> Server BlogPostAPI
+blogPostServer = undefined
 
 
-data RepoInfo = RepoInfo
-  { name  :: Text
-  , owner :: Text
-  } deriving Generic
+
+type RepositoryAPI =
+  "repo" :> Get '[JSON] [Repository] :<|>
+  "repo" :> Capture "name" Text :> Get '[JSON] Repository :<|>
+  "repo" :> ReqBody '[JSON] CreateRepositoryData:> Post '[JSON] Repository
 
 
-instance FromJSON RepoInfo
-instance ToJSON RepoInfo
+data CreateRepositoryData = CreateRepositoryData
+  { name    :: Text
+  , owner   :: Text
+  } deriving (Generic, FromJSON, ToJSON)
 
 
-type BlogAPI =
-  "blog" :> "all-posts" :> Get '[JSON] [BlogPost] :<|>
-  "blog" :> "create" :> ReqBody '[JSON] PostInfo :> Post '[JSON] BlogPost :<|>
-  "blog" :> Capture "post-name" Text :> Get '[JSON] BlogPost
+repositoryServer :: ConnectionPool -> Server RepositoryAPI
+repositoryServer = undefined
 
 
-type RepoAPI =
-  "repo" :> "all-repos" :> Get '[JSON] [Repository] :<|>
-  "repo" :> "create" :> ReqBody '[JSON] RepoInfo :> Post '[JSON] Repository :<|>
-  "repo" :> Capture "repo-name" Text :> Get '[JSON] Repository
+
+type WebsiteAPI = BlogPostAPI :<|> RepositoryAPI
 
 
-type SiteAPI = BlogAPI :<|> RepoAPI
+websiteServer :: ConnectionPool -> Server WebsiteAPI
+websiteServer pool = blogPostServer pool :<|> repositoryServer pool
 
 
-getBlogPostH :: ConnectionPool -> Text -> Handler BlogPost
-getBlogPostH pool postName = do
-  post <- liftIO $ flip runSqlPersistMPool pool $
-    selectFirst [ BlogPostShortTitle ==. postName ] [ ]
-
-  case post of
-    (Just post') -> return $ entityVal post'
-    Nothing      -> throwError err404
+websiteApp :: ConnectionPool -> Application
+websiteApp = serve (Proxy :: Proxy WebsiteAPI) . websiteServer
 
 
-getAllPostsH :: ConnectionPool -> Handler [BlogPost]
-getAllPostsH pool = do
-  posts <- liftIO $ flip runSqlPersistMPool pool $
-    selectList [ ] [ ]
-  return $ entityVal <$> posts
+mkWebsiteApp_ :: IO (ConnectionPool, Application)
+mkWebsiteApp_ = undefined
 
 
-postBlogPostH :: ConnectionPool -> PostInfo -> Handler BlogPost
-postBlogPostH pool info = do
-  now <- liftIO getCurrentTime
-
-  let title'   = title info
-  let short    = Data.Text.replace " " "_" title'
-  let content' = content info
-
-  let post     = BlogPost title' short content' now now
-
-  _ <- liftIO $ flip runSqlPersistMPool pool $ insert post
-
-  return post
-
-
-getRepoInfoH :: ConnectionPool -> Text -> Handler Repository
-getRepoInfoH pool repoName = do
-  repo <- liftIO $ flip runSqlPersistMPool pool $
-    selectFirst [ RepositoryName ==. repoName ] [ ]
-
-  case repo of
-    (Just repo') -> return $ entityVal repo'
-    Nothing      -> throwError err404
-
-
-getAllReposH :: ConnectionPool -> Handler [Repository]
-getAllReposH pool = do
-  repos <- liftIO $ flip runSqlPersistMPool pool $
-    selectList [ ] [ ]
-  return $ entityVal <$> repos
-
-
-postRepoInfoH :: ConnectionPool -> RepoInfo -> Handler Repository
-postRepoInfoH pool info = do
-  let name'  = name info
-  let owner' = owner info
-  let url    = Data.Text.concat [ "https://github.com" , "/" , name' , "/" , owner' ]
-  let repo   = Repository name' owner' url 0 0
-
-  _ <- liftIO $ flip runSqlPersistMPool pool $ insert repo
-
-  return repo
-
-
-siteAPI :: Proxy SiteAPI
-siteAPI = Proxy
-
-
-siteServer :: ConnectionPool -> Server SiteAPI
-siteServer pool = blogServer :<|> repoServer
-  where
-    blogServer = getAllPostsH pool :<|> postBlogPostH pool :<|> getBlogPostH pool
-    repoServer = getAllReposH pool :<|> postRepoInfoH pool :<|> getRepoInfoH pool
-
-
-siteApp :: ConnectionPool -> Application
-siteApp = serve siteAPI . siteServer
-
-
-mkSiteApp :: IO Application
-mkSiteApp = do
-  pool <- runStderrLoggingT $
-    createSqlitePool "db.sqlite" 5
-
-  time <- getCurrentTime
-
-  flip runSqlPool pool $ do
-    runMigration migrateAll
-    insert $ BlogPost "Haskell Is Simple" "haskell-is-simple" "SOON™" time time
-    insert $ BlogPost "Python Is Awesome" "python-is-awesome" "SOON™" time time
-    insert $ Repository "amalgam-lisp" "PureFunctor" "https://github.com/PureFunctor/amalgam-lisp" 3 453
-    insert $ Repository "purefunctor.me" "PureFunctor" "https://github.com/PureFunctor/purefunctor.me" 3 149
-
-  return $ siteApp pool
+mkWebsiteApp :: IO Application
+mkWebsiteApp = snd <$> mkWebsiteApp_
 
 
 debug :: IO ()
-debug = run 3000 =<< mkSiteApp
+debug = do
+  (pool, app) <- mkWebsiteApp_
+  return ()
