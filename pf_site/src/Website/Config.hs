4b93823312b7325d0b442cb438461918bac3b3b2
PureFunctor <purefunctor@gmail.com>
2021-01-26 00:30:50 +0800

    Add adminUser and adminPass to the configuration

--- pf_site/src/Website/Config.hs
+++ pf_site/src/Website/Config.hs
@@ -9,6 +9,8 @@ import System.Environment (getEnv)
 
 data Configuration = Configuration
   { jwtSecret  :: Text
+  , adminUser  :: Text
+  , adminPass  :: Text
   , connection :: ConnectionPool
   } deriving Show
 
@@ -17,9 +19,12 @@ mkConfiguration :: IO Configuration
 mkConfiguration = do
   jwt <- pack <$> getEnv "JWT_SECRET"
   
+  adu <- pack <$> getEnv "ADMIN_USER"
+  adp <- pack <$> getEnv "ADMIN_PASS"
+  
   dbn <- pack <$> getEnv "DB_FILE"
   dbc <- read <$> getEnv "DB_CONN"
   
   con <- runStderrLoggingT $ createSqlitePool dbn dbc
   
-  return $ Configuration jwt con
+  return $ Configuration jwt adu adp con
