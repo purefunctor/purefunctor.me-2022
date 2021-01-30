ba3106dec0f7e61bd68609ed03279254727267c6
PureFunctor <purefunctor@gmail.com>
2021-01-30 14:40:13 +0800

    Create and utilize the MutableRepositoryData type

--- pf_site/src/Website/API/Repo.hs
+++ pf_site/src/Website/API/Repo.hs
@@ -1,15 +1,21 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveAnyClass    #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE DeriveAnyClass         #-}
+{-# LANGUAGE DeriveGeneric          #-}
+{-# LANGUAGE DuplicateRecordFields  #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE TemplateHaskell        #-}
+{-# LANGUAGE TypeOperators          #-}
 module Website.API.Repo where
+
+import Control.Lens
 
 import Control.Monad ( void )
 import Control.Monad.IO.Class ( liftIO )
 import Control.Monad.Reader ( asks )
 
 import Data.Aeson ( FromJSON, ToJSON )
+import Data.Aeson.TH ( defaultOptions, deriveJSON, fieldLabelModifier )
 
 import Data.Maybe ( fromMaybe )
 
@@ -26,6 +32,7 @@ import Servant
 import Servant.Auth
 import Servant.Auth.Server
 
+import Control.Applicative
 import Website.API.Auth
 import Website.API.Common
 import Website.Config
@@ -41,19 +48,24 @@ type RepositoryAPI =
       Capture "name" Text :> Get '[JSON] Repository :<|>
 
       ( Auth '[JWT, Cookie] LoginPayload :>
-        ( ReqBody '[JSON] CreateRepositoryData :> Post '[JSON] MutableEndpointResult
+        ( ReqBody '[JSON] MutableRepositoryData :> Post '[JSON] MutableEndpointResult
         )
       )
 
     )
 
 
-data CreateRepositoryData
-  = CreateRepositoryData
-      { name  :: Text
-      , owner :: Text
+data MutableRepositoryData
+  = MutableRepositoryData
+      { _name    :: Maybe Text
+      , _owner   :: Maybe Text
+      , _url     :: Maybe Text
+      , _stars   :: Maybe Int
+      , _commits :: Maybe Int
       }
-  deriving (FromJSON, Generic, ToJSON)
+
+deriveJSON (defaultOptions { fieldLabelModifier = tail }) ''MutableRepositoryData
+makeLenses ''MutableRepositoryData
 
 
 repositoryServer :: ServerT RepositoryAPI WebsiteM
@@ -79,15 +91,29 @@ repositoryServer = getRepositories :<|> getRepository :<|> mkRepository
         (Just repository') -> return $ entityVal repository'
         Nothing            -> throwError err404
 
-    mkRepository :: AuthResult LoginPayload -> CreateRepositoryData -> WebsiteM MutableEndpointResult
-    mkRepository (Authenticated login) (CreateRepositoryData name owner) = do
+    mkRepository :: AuthResult LoginPayload -> MutableRepositoryData -> WebsiteM MutableEndpointResult
+    mkRepository (Authenticated login) payload = do
       pool <- asks connPool
 
-      let url = Text.concat [ "https://github.com/" , owner , "/" , name ]
+      let autoUrl owner name = Text.concat [ "https://github.com" , owner , "/" , name ]
 
-      void $ liftIO $ flip runSqlPersistMPool pool $
-        insert $ Repository name owner url (-1) (-1)
+      let mRepo = Repository
+            <$> payload ^. name
+            <*> payload ^. owner
+            <*> ( payload ^. url <|>
+                  autoUrl
+                    <$> payload ^. owner
+                    <*> payload ^. name
+                )
+            <*> payload ^. stars
+            <*> payload ^. commits
 
-      return $ MutableEndpointResult 200 $ "Repository created: " <> name
+      case mRepo of
+
+        Just repo -> do
+          void $ liftIO $ flip runSqlPersistMPool pool $ insert repo
+          return $ MutableEndpointResult 200 $ "Repository created: " <> repositoryName repo
+
+        Nothing -> throwError err400
 
     mkRepository _ _ = throwError err401
