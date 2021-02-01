40c45ad76f4a0f370a7433f678e8717865200e1a
PureFunctor <purefunctor@gmail.com>
2021-02-02 01:25:25 +0800

    Use microaeson and text as JSON helpers for tests

--- pf_site/test/Test/Utils.hs
+++ pf_site/test/Test/Utils.hs
@@ -2,6 +2,8 @@ module Test.Utils where
 
 import Test.Hspec.Wai
 
+import Data.Aeson.Micro ( Value , encode )
+
 import           Data.ByteString ( ByteString )
 import qualified Data.ByteString.Lazy.Char8 as LazyBS
 
@@ -11,14 +13,5 @@ import Network.HTTP.Types ( Header, methodPost )
 import Network.Wai.Test ( SResponse )
 
 
-type KeyValue = (String, String)
-
-
-postJSON :: ByteString -> [Header] -> [KeyValue] -> WaiSession st SResponse
-postJSON path headers json = request methodPost path headers json'
-  where
-    toContents :: (String, String) -> String
-    toContents = uncurry (printf "%s: %s")
-
-    json' :: LazyBS.ByteString
-    json' = LazyBS.pack $ "{" ++ concat (toContents <$> json) ++ "}"
+postJSON :: ByteString -> [Header] -> Value -> WaiSession st SResponse
+postJSON path headers = request methodPost path headers . encode
