bedec72c4ac1d8b119796f47c524c43f0c2f8c1b
PureFunctor <purefunctor@gmail.com>
2021-02-03 18:19:14 +0800

    Add the Data module to be shared among tests

---
+++ pf_site/test/Test/Data.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Test.Data where
+
+import Data.Time
+import Data.Time.Calendar.Julian
+
+import Website.Models
+
+
+repos :: [Repository]
+repos = 
+  [ Repository "amalgam-lisp" "PureFunctor" "https://github.com/PureFunctor/amalgam-lisp" 0 0
+  , Repository "purefunctor.me" "PureFunctor" "https://github.com/PureFunctor/purefunctor.me" 0 0
+  ]
+
+
+posts :: [BlogPost]
+posts =
+  [ BlogPost "Haskell Is Simple" "haskell-is-simple" "SOON™" now next
+  , BlogPost "Python Is Awesome" "python-is-awesome" "SOON™" now next
+  ]
+  where
+    now = UTCTime (fromJulian 2020 02 02) (secondsToDiffTime 0)
+    next = UTCTime (fromJulian 2020 02 03) (secondsToDiffTime 0)
