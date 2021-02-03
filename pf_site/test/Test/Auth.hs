37dba6a9c10bb9bb439a37c80075d49325fa39d6
PureFunctor <purefunctor@gmail.com>
2021-02-03 10:18:15 +0800

    Add test to check if Set-Cookie headers are set

--- pf_site/test/Test/Auth.hs
+++ pf_site/test/Test/Auth.hs
@@ -2,12 +2,17 @@
 module Test.Auth where
 
 import Test.Hspec
-import Test.Hspec.Wai
+import Test.Hspec.Wai as WaiTest
 import Test.Utils
 
 import Data.Aeson.Micro
 
+import qualified Data.ByteString as BS
+
 import Network.Wai
+import Network.Wai.Test as WaiTest
+
+import Web.Cookie
 
 import Website.Config
 
@@ -22,5 +27,10 @@ testAuth config app = with (pure app) $ do
     let headers =
           [ ("Content-Type", "application/json")
           ]
+
     it "should return 204 on a successful login" $ do
       postJSON "/login" headers value `shouldRespondWith` 204
+
+    it "should set the appropriate cookies" $ do
+      setCookies <- parseSetCookies <$> postJSON "/login" headers value
+      (setCookieName <$> setCookies) `shouldContain'` ["JWT-Cookie", "XSRF-TOKEN"]
--- pf_site/test/Test/Utils.hs
+++ pf_site/test/Test/Utils.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Test.Utils where
 
+import Test.Hspec
 import Test.Hspec.Wai
+
+import Control.Monad.IO.Class ( MonadIO )
 
 import Data.Aeson.Micro ( Value, encode )
 
@@ -10,8 +14,21 @@ import qualified Data.ByteString.Lazy.Char8 as LazyBS
 import Text.Printf
 
 import Network.HTTP.Types ( Header, methodPost )
-import Network.Wai.Test ( SResponse )
+import Network.Wai.Test ( SResponse(simpleHeaders) )
+
+import Web.Cookie ( SetCookie(setCookieName), parseSetCookie )
 
 
 postJSON :: ByteString -> [Header] -> Value -> WaiSession st SResponse
 postJSON path headers = request methodPost path headers . encode
+
+
+shouldContain' :: (MonadIO m, Show a, Eq a) => [a] -> [a] -> m ()
+shouldContain' lhs rhs = liftIO $ lhs `shouldContain` rhs
+
+
+parseSetCookies :: SResponse -> [SetCookie]
+parseSetCookies
+  = fmap (parseSetCookie . snd)
+  . filter ((== "Set-Cookie") . fst)
+  . simpleHeaders
