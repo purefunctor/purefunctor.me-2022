eb6b09a5aa53b14cb6c00e29aef0d46a9d94f528
PureFunctor <purefunctor@gmail.com>
2021-02-04 12:05:46 +0800

    Format and perform tests for POSTing blog posts

--- pf_site/test/Test/Blog.hs
+++ pf_site/test/Test/Blog.hs
@@ -3,7 +3,15 @@ module Test.Blog where
 
 import Control.Monad
 
+import Data.Aeson
+
 import Data.Text.Encoding
+
+import Data.Time
+import Data.Time.Calendar.Julian
+
+import           Database.Persist.Sqlite ( (==.) )
+import qualified Database.Persist.Sqlite as Sqlite
 
 import Test.Data
 import Test.Hspec
@@ -12,27 +20,71 @@ import Test.Utils
 
 import Network.Wai
 
+import Website.API.Blog
 import Website.Config
 import Website.Models
 
 
 testBlog :: Configuration -> Application -> Spec
 testBlog config app = with (pure app) $ do
+
   describe "GET /blog" $ do
     it "should return all blog posts" $ do
       get "/blog" `shouldRespondWith` matchCodeJSON 200 posts
+
     it "should return a specific post" $ do
       forM_ posts $ \post' -> do
         let endpoint = "/blog/" <> encodeUtf8 (blogPostShortTitle post')
         get endpoint `shouldRespondWith` matchCodeJSON 200 post'
 
   describe "POST /blog" $ do
+    let lTitle  = "Testing With Hspec"
+    let sTitle  = "testing-with-hspec"
+    let pBody   = "Nothing to see yet"
+    let tNow    = UTCTime (fromJulian 2020 02 02) (secondsToDiffTime 0)
+    let tNext   = UTCTime (fromJulian 2020 02 03) (secondsToDiffTime 0)
+
+    let newPost = MutableBlogPostData
+          (Just lTitle) (Just sTitle) (Just pBody) (Just tNow) (Just tNext)
+
+    let loginPayload = object
+          [ "username" .= adminUser config
+          , "password" .= adminPass config
+          ]
+
     it "should require authentication " $ do
-      WaiTest.pendingWith "POST /blog"
+      postJSON "/blog" [] (toJSON newPost) `shouldRespondWith` 401
+
     it "should mutate the database" $ do
-      WaiTest.pendingWith "POST /blog"
+      mAuthHeaders <-
+        mkAuthHeaders . parseSetCookies <$> postJSON "/login" [] loginPayload
+
+      case mAuthHeaders of
+        Just authHeaders -> do
+          postJSON "/blog" authHeaders (toJSON newPost) `shouldRespondWith` 200
+
+          inDB <-  WaiTest.liftIO $ flip Sqlite.runSqlPool (connPool config) $ do
+            Sqlite.exists [ BlogPostShortTitle ==. sTitle ]
+
+          inDB `shouldBe'` True
+
+        Nothing -> fail "failed to create authentication headers"
+
     it "should require title and contents" $ do
-      WaiTest.pendingWith "POST /blog"
+      let invalid =
+            [ newPost { _title = Nothing }
+            , newPost { _contents = Nothing }
+            ]
+
+      mAuthHeaders <-
+        mkAuthHeaders . parseSetCookies <$> postJSON "/login" [] loginPayload
+
+      case mAuthHeaders of
+        Just authHeaders ->
+          forM_ invalid $ \payload -> do
+            postJSON "/blog" authHeaders (toJSON payload) `shouldRespondWith` 400
+
+        Nothing -> fail "failed to create authentication headers"
 
   describe "PUT /blog" $ do
     it "should require authentication " $ do
