d4f973739c0f7c5f8d3c799a70b89eb23fe3e915
PureFunctor <purefunctor@gmail.com>
2021-02-18 09:13:29 +0800

    Create JSON codecs for parsing resources

--- pf_app/src/Data/Resources.purs
+++ pf_app/src/Data/Resources.purs
@@ -1,5 +1,10 @@
 module PF.Data.Resources where
 
+import Prelude
+
+import Data.Codec.Argonaut as CA
+import Data.Codec.Argonaut.Record as CAR
+
 
 type BlogPost =
   { fullTitle :: String
@@ -17,3 +22,25 @@ type Repository =
   , stars :: Int
   , commits :: Int
   }
+
+
+blogPostCodec :: CA.JsonCodec BlogPost
+blogPostCodec =
+  CA.object "BlogPost" $ CAR.record
+    { fullTitle:  CA.string
+    , shortTitle: CA.string
+    , contents: CA.string
+    , published: CA.string
+    , updated: CA.string
+    }
+
+
+repositoryCodec :: CA.JsonCodec Repository
+repositoryCodec =
+  CA.object "Repository" $ CAR.record
+    { name: CA.string
+    , owner: CA.string
+    , url: CA.string
+    , stars: CA.int
+    , commits: CA.int
+    }
