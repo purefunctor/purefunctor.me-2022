8e2debf6152fdaa715dcd8a0d7d867490b4c7eb2
PureFunctor <purefunctor@gmail.com>
2021-02-20 12:38:04 +0800

    Add initial repository card component

---
+++ pf_app/src/Component/RepoCard.purs
@@ -0,0 +1,71 @@
+module Website.Component.RepoCard where
+
+import Prelude
+
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+import Website.Component.Utils (css)
+import Website.Data.Resources (Repository)
+
+
+type Input = Array Repository
+type State = Array Repository
+
+
+component :: forall query output m. H.Component HH.HTML query Input output m
+component =
+  H.mkComponent
+  { initialState
+  , render
+  , eval: H.mkEval $ H.defaultEval
+  }
+
+
+initialState :: Input -> State
+initialState = identity
+
+
+render :: forall action slots m. State -> H.ComponentHTML action slots m
+render repositories =
+  HH.div
+  [ css "h-full flex flex-wrap justify-center" ]
+  cards
+  where
+    makeCard :: forall w i. Repository -> HH.HTML w i
+    makeCard repository =
+      HH.a
+      [ css "md:w-1/3 w-full h-40 flex flex-col flex-none bg-white rounded-xl divide-solid divide-y-2 md:shadow-lg shadow-md m-2 transform hover:-translate-y-2 cursor-pointer"
+      , HP.href repository.url
+      ]
+      [ HH.div
+        [ css "flex p-4 text-2xl" ]
+        [ HH.div
+          [ css "flex-grow truncate" ]
+          [ HH.text $ repository.name
+          ]
+        , HH.i [ css "fab fa-github" ] [ ]
+        ]
+      , HH.div
+        [ css "flex flex-col h-full p-4" ]
+        [ HH.div
+          [ css "flex-grow truncate text-md" ]
+          [ HH.text $ repository.description
+          ]
+        , HH.div
+          [ css "flex" ]
+          [ HH.div
+            [ css "flex-grow" ]
+            [ HH.text $ show repository.stars <> " "
+            , HH.i [ css "far fa-star" ] [ ]
+            ]
+          , HH.div_
+            [ HH.text $ show repository.commits <> " "
+            , HH.i [ css "fas fa-history" ] [ ]
+            ]
+          ]
+        ]
+       ]
+
+    cards :: forall w i. Array (HH.HTML w i)
+    cards = makeCard <$> repositories
--- pf_app/src/Pages/Home.purs
+++ pf_app/src/Pages/Home.purs
@@ -3,16 +3,20 @@ module Website.Pages.Home where
 import Prelude
 
 import Data.Maybe (Maybe(..))
-import Effect.Class.Console (log)
+import Data.Symbol (SProxy(..))
 import Effect.Aff.Class (class MonadAff)
+import Effect.Class.Console (log)
 import Halogen as H
 import Halogen.HTML as HH
 import Website.Capability.Resources (class ManageRepository, getRepositories)
+import Website.Component.RepoCard as RepoCard
 import Website.Component.Utils (css)
 
 
 type State = Unit
 data Action = Initialize
+type ChildSlots =
+  ( projects :: forall query. H.Slot query Void Unit )
 
 
 component
@@ -40,7 +44,7 @@ render
      MonadAff m
   => ManageRepository m
   => State
-  -> H.ComponentHTML Action () m
+  -> H.ComponentHTML Action ChildSlots m
 render _ =
   HH.div [ css "bg-gray-100 h-screen overflow-auto scroll-snap-y-mandatory"  ]
   [ HH.div [ css "h-auto w-full lg:w-11/12 mx-auto" ]
@@ -58,8 +62,8 @@ render _ =
     , subsection "About" [ css "text-lg p-5" ]
       [ HH.text "Text"
       ]
-    , subsection "Projects" [ css "text-lg p-5" ]
-      [ HH.text "Text"
+    , subsection "Projects" [ css "p-5" ]
+      [ HH.slot ( SProxy :: SProxy "projects" ) unit RepoCard.component exampleRepositories absurd
       ]
     , subsection "Contact" [ css "text-lg p-5" ]
       [ HH.text "Text"
@@ -75,13 +79,37 @@ render _ =
       , HH.div props content
       ]
 
+    exampleRepositories =
+      [ { name: "amalgam-lisp"
+        , owner: "PureFunctor"
+        , commits: 453
+        , stars: 3
+        , description: "LISP-like interpreted language implemented in Python."
+        , url: "https://github.com/PureFunctor/amalgam-lisp"
+        }
+      , { name: "purefunctor.me"
+        , owner: "PureFunctor"
+        , commits: 333
+        , stars: 1
+        , description: "My personal portfolio website written in PureScript and Haskell"
+        , url: "https://github.com/PureFunctor/purefunctor.me"
+        }
+      , { name: "dotfiles"
+        , owner: "PureFunctor"
+        , commits: 28
+        , stars: 0
+        , description: "Personal dotfiles."
+        , url: "https://github.com/PureFunctor/dotfiles"
+        }
+      ]
+
 
 handleAction
   :: forall output m.
      MonadAff m
   => ManageRepository m
   => Action
-  -> H.HalogenM State Action () output m Unit
+  -> H.HalogenM State Action ChildSlots output m Unit
 handleAction = case _ of
   Initialize -> do
     mRepositories <- getRepositories
