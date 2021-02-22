b90cac2c08ba6a7e2973fef5c9a1922505270cfc
PureFunctor <purefunctor@gmail.com>
2021-02-22 10:31:45 +0800

    Install and configure static-haskell-nix

--- config.nix
+++ config.nix
@@ -10,75 +10,88 @@ let
   gitignore = import sources."gitignore.nix" { };
   inherit (gitignore) gitignoreFilter;
 
-  nixpkgs = import (fetchTarball sources.nixpkgs.url) { };
-  hsStaticPkgs = nixpkgs.pkgsMusl.haskell.packages."${compiler}".extend (
-    let
-      pkgs = nixpkgs.pkgsMusl;
-
-      collapseOverrides = 
-        pkgs.lib.fold pkgs.lib.composeExtensions (_: _: {});
-
-      manualOverrides = self: super: {
-        # Make sure that our project has its own derivation.
-        purefunctor-me = 
-          let
-            check = if doCheck then pkgs.haskell.lib.doCheck else pkgs.lib.id;
-  
-            srcFilter = src:
+  config = {
+    packageOverrides = pkgs: rec {
+      haskell = pkgs.haskell // {
+        packages = pkgs.haskell.packages // {
+          "${compiler}" = pkgs.haskell.packages."${compiler}".override {
+            overrides = 
               let
-                srcIgnored = gitignoreFilter src;
+                collapseOverrides = 
+                  pkgs.lib.fold pkgs.lib.composeExtensions (_: _: {});
 
-                dontIgnore = [
-                  "config.toml"
-                ];
+                manualOverrides = self: super: {
+                  # Make sure that our project has its own derivation.
+                  purefunctor-me = 
+                    let
+                      check = if doCheck then haskell.lib.doCheck else pkgs.lib.id;
+  
+                      srcFilter = src:
+                        let
+                          srcIgnored = gitignoreFilter src;
+
+                          dontIgnore = [
+                            "config.toml"
+                          ];
+                        in
+                          path: type:
+                            srcIgnored path type
+                              || builtins.elem (builtins.baseNameOf path) dontIgnore;
+
+                      src = pkgs.lib.cleanSourceWith {
+                        filter = srcFilter ./.;
+                        src = ./.;
+                        name = "purefunctor-me-src";
+                      };
+
+                      full = super.callCabal2nix "purefunctor-me" src { };
+
+                      minimal = pkgs.haskell.lib.overrideCabal
+                      full
+                      ( _: { 
+                        enableSharedExecutables = false;
+                        enableSharedLibraries = false;
+                        configureFlags = [
+                          "--ghc-option=-optl=-static"
+                          "--ghc-option=-optl=-pthread"
+                          "--ghc-option=-optl=-L${pkgs.gmp6.override { withStatic = true; }}/lib"
+                          "--ghc-option=-optl=-L${pkgs.zlib.static}/lib"
+                          "--ghc-option=-optl=-L${pkgs.glibc.static}/lib"
+                        ];
+                      } );
+
+                    in
+                      check (if doMinimal then minimal else full);
+
+                  # Disable tests and benchmarks for all packages.
+                  mkDerivation = args: super.mkDerivation ({
+                    doCheck = false;
+                    doBenchmark = false;
+                    doHoogle = true;
+                    doHaddock = true;
+                    enableLibraryProfiling = false;
+                    enableExecutableProfiling = false;
+                  } // args);
+                };
               in
-                path: type:
-                  srcIgnored path type
-                    || builtins.elem (builtins.baseNameOf path) dontIgnore;
-
-            src = pkgs.lib.cleanSourceWith {
-              filter = srcFilter ./.;
-              src = ./.;
-              name = "purefunctor-me-src";
-            };
-
-            full = super.callCabal2nix "purefunctor-me" src { };
-
-            minimal = pkgs.haskell.lib.overrideCabal
-            full
-            ( _: { 
-              enableSharedExecutables = false;
-              enableSharedLibraries = false;
-              configureFlags = [
-                "--ghc-option=-optl=-static"
-                "--ghc-option=-optl=-pthread"
-                "--ghc-option=-optl=-L${pkgs.glibc.static}/lib"
-                "--ghc-option=-optl=-L${pkgs.gmp6.override { withStatic = true; }}/lib"
-                "--ghc-option=-optl=-L${pkgs.libffi.overrideAttrs ( _ : { dontDisableStatic = true; } )}"
-                "--ghc-option=-optl=-L${pkgs.libidn2.overrideAttrs ( _ : { dontDisableStatic = true; } )}"
-                "--ghc-option=-optl=-L${pkgs.libunistring.overrideAttrs ( _ : { dontDisableStatic = true; } )}"
-                "--ghc-option=-optl=-L${pkgs.sqlite.overrideAttrs ( _ : { dontDisableStatic = true; } )}"
-                "--ghc-option=-optl=-L${pkgs.zlib.static}/lib"
-              ];
-            } );
-
-          in
-            check (if doMinimal then minimal else full);
-
-        # Disable tests and benchmarks for all packages.
-        mkDerivation = args: super.mkDerivation ({
-          doCheck = false;
-          doBenchmark = false;
-          doHoogle = true;
-          doHaddock = true;
-          enableLibraryProfiling = false;
-          enableExecutableProfiling = false;
-        } // args);
+                collapseOverrides [ manualOverrides ];
+          };
+        };
       };
-    in
-      collapseOverrides [ manualOverrides ] );
+    };
+  };
 
+  nixpkgs = import (fetchTarball sources.nixpkgs.url) { inherit config; };
   easy-purescript-nix = import sources.easy-purescript-nix { pkgs = nixpkgs; };
+
+  defaultCabalPackageVersionComingWithGhc = { ghc884 = "Cabal_3_2_1_0"; }."${compiler}";
+
+  static-haskell-nix = fetchTarball sources.static-haskell-nix.url;
+
+  survey = import "${static-haskell-nix}/survey" { 
+    inherit compiler defaultCabalPackageVersionComingWithGhc;
+    normalPkgs = nixpkgs; 
+  };
 in
-  { inherit compiler nixpkgs hsStaticPkgs easy-purescript-nix;
+  { inherit compiler nixpkgs easy-purescript-nix survey;
   }
--- release.nix
+++ release.nix
@@ -6,5 +6,5 @@ let
   config = import ./config.nix { inherit doCheck doMinimal; };
 
 in
-  { purefunctor-me = config.hsStaticPkgs.purefunctor-me;
+  { purefunctor-me = config.survey.haskellPackages.purefunctor-me;
   }
