533a48b268fa7868c5bba86da46c1e6f4207e068
PureFunctor <purefunctor@gmail.com>
2021-02-24 01:49:07 +0800

    Remove Nix-based builds for the frontend

--- docker-frontend.nix
+++
@@ -1,9 +0,0 @@
-let
-  config = import ./config.nix { };
-  inherit (config) nixpkgs;
-in
-  nixpkgs.dockerTools.streamLayeredImage {
-    name = "site-frontend";
-    tag = "latest";
-    contents = import ./release-frontend.nix;
-  }
