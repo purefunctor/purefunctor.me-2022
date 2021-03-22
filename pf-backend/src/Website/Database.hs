676420197d46a077e8ff41d2ebd8f50de47f8091
PureFunctor <purefunctor@gmail.com>
2021-03-22 13:26:03 +0800

    Implement the runMigration utility function

--- pf-backend/src/Website/Database.hs
+++ pf-backend/src/Website/Database.hs
@@ -2,17 +2,25 @@
 module Website.Database
   ( module Website.Database.Models
   , module Website.Database.Pool
+  , runMigration
   , WebsiteDb
   , websiteDb
   , posts
   , repos
   ) where
 
+import Control.Exception
+
+import qualified Data.Text.IO as TIO
+
 import Database.Beam
 import Database.Beam.Sqlite
+import Database.SQLite.Simple
 
 import Website.Database.Models
 import Website.Database.Pool
+
+import Paths_purefunctor_me
 
 
 data WebsiteDb f = WebsiteDb
@@ -25,3 +33,16 @@ websiteDb = defaultDbSettings
 
 WebsiteDb (TableLens posts)
           (TableLens repos) = dbLenses
+
+
+runMigration :: MonadIO m => ConnPool -> m ()
+runMigration = liftIO . flip withConnPool runMigration_
+  where
+    runMigration_ conn = do
+      defaultMigration <- getDataFileName "migration.sql"
+
+      migration <-
+        catch (TIO.readFile "migration.sql")
+        (\(SomeException _) -> TIO.readFile defaultMigration)
+
+      execute_ conn (Query migration)
