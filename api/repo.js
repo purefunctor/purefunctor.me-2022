807fbcdd6cce96316f27570a0ee5cb09ccd57e1c
PureFunctor <purefunctor@gmail.com>
2021-04-17 22:31:29 +0800

    Use dotenv to load secrets

--- api/repo.js
+++ api/repo.js
@@ -2,32 +2,37 @@
 
 const got = require("got");
 
+require("dotenv").config();
+
 function getRepositoryUrl(owner, repository) {
   return `https://api.github.com/repos/${owner}/${repository}`;
-}
+};
 
 async function getRepositoryData(owner, repository) {
   const baseUrl = getRepositoryUrl(owner, repository);
   const languagesUrl = baseUrl + "/languages";
   const participationUrl = baseUrl + "/stats/participation";
 
-  const basic = await got(baseUrl).then(
-    value => JSON.parse(value.body)
-  );
-  const languages = await got(languagesUrl).then(
-    value => JSON.parse(value.body)
-  );
-  const participation = await got(participationUrl).then(
-    value => JSON.parse(value.body)
-  );
+  const gh = async (url) => {
+    return got(url, {
+      username: process.env.GITHUB_USER,
+      password: process.env.GITHUB_SECRET,
+    }).then(
+      value => JSON.parse(value.body)
+    );
+  };
+
+  const basic = await gh(baseUrl);
+  const languages = await gh(languagesUrl);
+  const participation = await gh(participationUrl);
 
   return {
     description: basic["description"],
-    languages: languages,
     stars: basic["stargazers_count"],
     commits: participation["all"],
+    languages: languages,
   };
-}
+};
 
 module.exports = (_, res) => {
   getRepositoryData("PureFunctor", "purefunctor.me").then(
