37f2fba3d11377de529665796ed0ba3a61c54df0
PureFunctor <purefunctor@gmail.com>
2021-04-25 18:21:36 +0800

    Create navbar skeleton

--- src/Pages/Home.purs
+++ src/Pages/Home.purs
@@ -49,39 +49,43 @@ render
   -> H.ComponentHTML action ChildSlots m
 render _ =
   HH.div
-  [ css'
-    [ "bg-faint h-screen overflow-auto"
-    , "lg:scroll-snap-y-proximity no-scroll-snap-type"
-    ]
+  [ css "bg-faint h-screen overflow-auto lg:scroll-snap-y-proximity no-scroll-snap-type"
   ]
   [ HH.div [ css "h-auto w-full lg:w-11/12 mx-auto" ]
-    [ HH.section
-      [ css'
-        [ "h-screen flex flex-col"
-        , "justify-center items-center space-y-5"
-        , "lg:scroll-snap-align-start no-scroll-snap-align"
+    [ HH.section [ css "h-screen flex flex-col" ]
+      [ HH.div [ css "lg:scroll-snap-align-start no-scroll-snap-align" ]
+        [ HH.nav [ css "flex justify-between items-center px-8 h-32" ]
+          [ HH.span [ css "space-x-6" ]
+            [ picture "inline self-center rounded-full" 32 32
+            , HH.p [ css "inline" ]
+              [ HH.text "Pure's Website"
+              ]
+            ]
+          , HH.ul [ css "space-x-6" ]
+            [ navLink "About"
+            , navLink "Projects"
+            , navLink "Contact"
+            , navLink "Blog"
+            ]
+          ]
+        ]
+
+      , HH.section [ css "flex-grow flex mx-auto items-center" ]
+        [ picture "h-56 w-56 rounded-full shadow-xl ring-2 ring-black" 256 256
+        ]
+
+      , HH.div [ css "h-32" ]
+        [ -- Flexbox centering hack.
         ]
       ]
-      [ HH.img
-        [ css "h-56 w-56 rounded-full shadow-xl ring-2 ring-black"
-        , HP.src "https://avatars.githubusercontent.com/u/66708316?v=4"
-        , HP.width 256
-        , HP.height 256
-        , HP.alt "GitHub Profile Picture"
-        ]
-      , HH.div [ css "text-4xl font-extralight text-center" ]
-        [ HH.text "PureFunctor"
-        ]
-      , HH.div [ css "text-4xl font-thin text-center" ]
-        [ HH.text "Student, Python, FP"
-        ]
-      ]
+
     , subsection "min-h-screen" "About"
-      [ AboutCard.element
-      ]
+      [ AboutCard.element ]
+
     , subsection "min-h-screen" "Projects"
       [ ProjectCards.make ( Proxy :: Proxy "projects" )
       ]
+
     , subsection "min-h-screen" "Contact"
       [ ContactCards.make ( Proxy :: Proxy "contacts" )
       ]
@@ -101,3 +105,17 @@ render _ =
         [ HH.h1_ [ HH.text title ]
         ]
       ] <> child
+
+    picture c w h =
+      HH.img
+      [ css c
+      , HP.src "https://avatars.githubusercontent.com/u/66708316?v=4"
+      , HP.width w
+      , HP.height h
+      , HP.alt "GitHub Profile Picture"
+      ]
+
+    navLink title =
+      HH.li [ css "inline text-underline cursor-pointer" ]
+      [ HH.text title
+      ]
