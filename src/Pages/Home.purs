181b09237ed51e9317f277568e3d13fbb04407b4
PureFunctor <purefunctor@gmail.com>
2021-04-27 12:24:47 +0800

    Use Sass for the home page

--- src/Pages/Home.purs
+++ src/Pages/Home.purs
@@ -6,12 +6,12 @@ import Effect.Aff.Class (class MonadAff)
 import Effect.Class (class MonadEffect)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Halogen.HTML.Properties.ARIA as HPA
 import Website.Capability.Navigation (class Navigate, navigate)
 import Website.Capability.Resources (class ManageRepository)
-import Website.Component.HTML.Navbar (navbar)
-import Website.Component.HTML.ProfilePicture (profilePicture)
-import Website.Component.Utils (css)
-import Website.Data.Routes (Route)
+import Website.Data.Routes (Route(..))
 
 
 data Action
@@ -49,21 +49,50 @@ render
   => State
   -> H.ComponentHTML Action slots m
 render _ =
-  HH.div
-  [ css "bg-faint h-screen overflow-auto lg:scroll-snap-y-proximity no-scroll-snap-type"
-  ]
-  [ HH.section [ css "flex flex-col h-full w-full lg:w-11/12 mx-auto" ]
-    [ navbar Navigate
-
-    , HH.section [ css "flex-grow flex mx-auto items-center" ]
-      [ profilePicture "h-56 w-56 rounded-full shadow-xl ring-2 ring-black" 256 256
+  HH.div [ HP.id "home-page" ]
+  [ HH.section [ HP.id "home-base" ]
+    [ HH.nav [ HP.id "home-top" ]
+      [ HH.span_
+        [ HH.img
+          [ HP.src "https://avatars.githubusercontent.com/u/66708316?v=4"
+          , HP.width 32
+          , HP.height 32
+          , HP.alt "GitHub Profile Picture"
+          ]
+        , navItem "Pure's Website" HomeR
+        ]
+      , HH.ul_
+        [ navItem "About" AboutR
+        , navItem "Projects" ContactR
+        , navItem "Contact" ProjectsR
+        , HH.a [ HP.href "https://blog.purefunctor.me" ]
+          [ HH.text "Blog"
+          ]
+        ]
       ]
-
-    , HH.div [ css "h-32" ]
-      [ -- Flexbox centering hack.
+    , HH.section [ HP.id "home-center" ]
+      [ HH.img
+        [ HP.src "https://avatars.githubusercontent.com/u/66708316?v=4"
+        , HP.width 256
+        , HP.height 256
+        , HP.alt "GitHub Profile Picture"
+        ]
+      ]
+     ,HH.div [ HP.id "home-bottom" ]
+      [
       ]
     ]
   ]
+  where
+    navItem title route =
+      HH.button
+      [ HE.onClick \_ -> Navigate route
+      , HP.tabIndex 0
+      , HPA.role "link"
+      , HPA.label $ "Navigate to " <> title <> " page"
+      ]
+      [ HH.text title
+      ]
 
 
 handleAction
