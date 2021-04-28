ad785ca08ace10b73037a593fc51b34c04c6bb0b
PureFunctor <purefunctor@gmail.com>
2021-04-28 09:25:09 +0800

    Create independent navbar component

--- src/Pages/Home.purs
+++ src/Pages/Home.purs
@@ -5,25 +5,25 @@ import Prelude
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Halogen.HTML.Properties.ARIA as HPA
-import Website.Capability.Navigation (class Navigate, navigate)
+import Website.Capability.Navigation (class Navigate)
+import Website.Capability.OpenUrl (class OpenUrl)
 import Website.Capability.Resources (class ManageRepository)
-import Website.Data.Routes (Route(..))
-
-
-data Action
-  = Navigate Route
+import Website.Component.Navbar (WithNavbar, _navbar)
+import Website.Component.Navbar as Navbar
 
 
 type State = Unit
+
+type Slots = WithNavbar ()
 
 
 component
   :: forall query input output m.
      MonadAff m
   => ManageRepository m
+  => Navigate m
+  => OpenUrl m
   => H.Component query input output m
 component =
   H.mkComponent
@@ -38,33 +38,17 @@ initialState _ = unit
 
 
 render
-  :: forall slots m.
+  :: forall action m.
      MonadAff m
   => ManageRepository m
+  => Navigate m
+  => OpenUrl m
   => State
-  -> H.ComponentHTML Action slots m
+  -> H.ComponentHTML action Slots m
 render _ =
   HH.div [ HP.id "home-page" ]
   [ HH.section [ HP.id "home-base" ]
-    [ HH.nav [ HP.id "home-top" ]
-      [ HH.span_
-        [ HH.img
-          [ HP.src "https://avatars.githubusercontent.com/u/66708316?v=4"
-          , HP.width 32
-          , HP.height 32
-          , HP.alt "GitHub Profile Picture"
-          ]
-        , navItem "Pure's Website" HomeR
-        ]
-      , HH.ul_
-        [ navItem "About" AboutR
-        , navItem "Projects" ContactR
-        , navItem "Contact" ProjectsR
-        , HH.a [ HP.href "https://blog.purefunctor.me" ]
-          [ HH.text "Blog"
-          ]
-        ]
-      ]
+    [ HH.slot _navbar unit Navbar.component unit absurd
     , HH.section [ HP.id "home-center" ]
       [ HH.img
         [ HP.src "https://avatars.githubusercontent.com/u/66708316?v=4"
@@ -78,24 +62,3 @@ render _ =
       ]
     ]
   ]
-  where
-    navItem title route =
-      HH.button
-      [ HE.onClick \_ -> Navigate route
-      , HP.tabIndex 0
-      , HPA.role "link"
-      , HPA.label $ "Navigate to " <> title <> " page"
-      ]
-      [ HH.text title
-      ]
-
-
-handleAction
-  :: forall state slots output m
-   . MonadAff m
-  => Navigate m
-  => Action
-  -> H.HalogenM state Action slots output m Unit
-handleAction = case _ of
-  Navigate route -> do
-    navigate route
