7624845fd701180bb56fa230f59e84f774b5a3d9
PureFunctor <purefunctor@gmail.com>
2021-10-04 22:59:12 +0800

    Make build script work with subdirs

--- bin/static.js
+++ bin/static.js
@@ -1,13 +1,22 @@
 const fs = require("fs/promises");
+const glob = require("glob");
 const nunjucks = require("nunjucks");
+const path = require("path");
 
 nunjucks.configure(["html"]);
 
 async function main() {
     await fs.mkdir("./public", { recursive: true });
-    let pages = await fs.readdir("html/pages");
+    let pages = glob.sync("html/pages/**/*.html");
     pages.forEach(async page => {
-	let result = nunjucks.render(`./pages/${page}`);
+        page = page.split("/").slice(2).join("/");
+
+        let dir = path.parse(page).dir;
+        if (dir != "") {
+            await fs.mkdir(`./public/${dir}`, { recursive: true });
+        }
+
+        let result = nunjucks.render(`./pages/${page}`);
 	await fs.writeFile(`./public/${page}`, result);
     });
 }
