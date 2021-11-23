6ffd7adae13d154564009d0dcae7e8d2bea03547
PureFunctor <purefunctor@gmail.com>
2021-11-23 22:37:43 +0800

    Add OpenGraph meta tags

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -6,6 +6,12 @@ module.exports = {
   head: [
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16.png"}],
+    ['meta', { prefix: ogprefix, property: 'og:title', content: "PureFunctor" }],
+    ['meta', { prefix: ogprefix, property: 'twitter:title', content: "PureFunctor" }],
+    ['meta', { prefix: ogprefix, property: 'og:type', content: 'website' }],
+    ['meta', { prefix: ogprefix, property: 'og:url', content: 'https://purefunctor.me' }],
+    ['meta', { prefix: ogprefix, property: 'og:description', content: "PureFunctor's Home Page" }],
+    ['meta', { prefix: ogprefix, property: 'og:image', content: 'https://raw.githubusercontent.com/vtrl/.github/main/profile/banner.png' }],
   ],
   themeConfig: {
     base: "/site/",
